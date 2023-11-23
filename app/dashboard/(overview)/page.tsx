4d52a448f66015af5e3b61fe6d7a5f3e35f3facb
marcinstenka <mastenka@wp.pl>
2023-11-23 22:13:43 +0100

    chapter 9 completed

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,20 +1,14 @@
 import { Suspense } from 'react';
-import { Card } from '../../ui/dashboard/cards';
+import CardWrapper from '../../ui/dashboard/cards';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import { lusitana } from '../../ui/fonts';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
-import { fetchLatestInvoices, fetchCardData } from '../../lib/data';
+import { fetchCardData } from '../../lib/data';
 
 export default async function Page() {
-	const latestInvoices = await fetchLatestInvoices();
-	const {
-		numberOfCustomers,
-		numberOfInvoices,
-		totalPaidInvoices,
-		totalPendingInvoices,
-	} = await fetchCardData();
+	
 
 	return (
 		<main>
@@ -22,7 +16,7 @@ export default async function Page() {
 				Dashboard
 			</h1>
 			<div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-				{<Card title='Collected' value={totalPaidInvoices} type='collected' />}
+				{/* {<Card title='Collected' value={totalPaidInvoices} type='collected' />}
 				{<Card title='Pending' value={totalPendingInvoices} type='pending' />}
 				{
 					<Card
@@ -37,11 +31,18 @@ export default async function Page() {
 						value={numberOfCustomers}
 						type='customers'
 					/>
-				}
+				} */}
+				<Suspense fallback={<CardsSkeleton/>}>
+					<CardWrapper/>
+				</Suspense>
 			</div>
 			<div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-				<Suspense fallback={<RevenueChartSkeleton/>}><RevenueChart/> </Suspense>
-				{<LatestInvoices latestInvoices={latestInvoices} />}
+				<Suspense fallback={<RevenueChartSkeleton />}>
+					<RevenueChart />
+				</Suspense>
+				<Suspense fallback={<LatestInvoicesSkeleton />}>
+					<LatestInvoices />
+				</Suspense>
 			</div>
 		</main>
 	);
