8a82db45bcce320843d8c02f5aa46018c5deb098
marcinstenka <mastenka@wp.pl>
2023-12-27 14:07:19 +0100

    chapter 16 completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Customers(){
-    return <p>Customers Page</p>
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: 'Customers',
+};
+export default function Customers() {
+	return <p>Customers Page</p>;
 }
